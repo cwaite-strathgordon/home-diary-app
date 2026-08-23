58acfad5fe4d73db9d86bcb23bd02de6a310b4fc
Christopher Waite <cwaite@Christophers-MacBook-Air.local>
2026-08-23 10:17:02 +0100

    Add yaml for Amplify config

---
+++ src/environments/environment.production.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://api.homediary.app/api',
+  auth0: {
+    domain: 'YOUR_AUTH0_DOMAIN',
+    clientId: 'PRODUCTION_AUTH0_CLIENT_ID',
+    audience: 'https://api.homediary.app',
+  },
+};
---
+++ src/environments/environment.test.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: false,
+  apiUrl: 'https://test-api.homediary.app/api',
+  auth0: {
+    domain: 'YOUR_AUTH0_DOMAIN',
+    clientId: 'TEST_AUTH0_CLIENT_ID',
+    audience: 'https://test-api.homediary.app',
+  },
+};
