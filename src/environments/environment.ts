34ae6a69641dfcaf05e6fbf9a4695c789484abc9
Christopher Waite <cwaite@Mac.localdomain>
2026-08-28 17:55:58 +0200

    Add Auth0 config for new Auth0 account

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,8 @@ export const environment = {
   production: false,
   apiUrl: 'http://localhost:5001/api',
   auth0: {
-    domain:      'dev-pjaxwt6ra4imv2ac.us.auth0.com',
-    clientId:    'a48O1zmySQBJNRibmFDfvoFQV1IqDeL9',
+    domain:      'dev-izdhne4wmbzze2rf.us.auth0.com',
+    clientId:    'oyUnVqtDSwjkYhESoVTcfunjl9kL4VBv',
     audience:    'http://localhost:5001',
     redirectUri: 'http://localhost:4200',
   },
